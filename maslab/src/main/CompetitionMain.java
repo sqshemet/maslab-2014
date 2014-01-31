4a7259db6dbb421d0fb0040e0ea310a841f1a713
Sabrina Shemet <sqshemet@mit.edu>
2014-01-31 07:29:33 -0500

    More images and things.

--- maslab/src/main/CompetitionMain.java
+++ maslab/src/main/CompetitionMain.java
@@ -1,5 +1,10 @@
 package main;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.util.ArrayList;
 import java.util.Map;
 
 import org.opencv.core.Core;
@@ -23,15 +28,42 @@ public class CompetitionMain {
 		System.out.println("MAP --> " + botclient.getMap());
    //  Load the native library.    
 	    System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-	    vision.BlobDetect.viewStream(); //Comment this out if you don't want the stream
-	    while(botclient.gameStarted()){
-	    	Mat frame = vision.BlobDetect.getFrame();
-	    	//Mat frame = Highgui.imread("/home/sqshemet/blue_small.jpg", Highgui.CV_LOAD_IMAGE_COLOR);
-	    	Map.Entry<Point, Float> ball = vision.BlobDetect.getClosestBall(frame);
-	    	double distance = vision.BlobDetect.distanceToBall(ball.getValue());
-	    	Point center = ball.getKey();
-	    	int orientation = vision.BlobDetect.orientation(center);
-	    }
+	    //Code here
+		botclient.send("a", "State", "Ball Search");
+		botclient.send("b", "Gyro", "3.14159");
+		
+		// Smiley
+		ArrayList<Mat> mats = new ArrayList<Mat>();
+		ArrayList<BufferedImage> bufs = new ArrayList<BufferedImage>();
+		Mat image1 = Highgui.imread("/home/sqshemet/ariel1.jpg", Highgui.CV_LOAD_IMAGE_COLOR);
+		mats.add(image1);
+		for(Mat m : mats)
+			{
+			bufs.add(MatToBufferedImage(m));
+		}
+		
+		botclient.sendImage(bufs.get(1));
+	    
 		botclient.close();
-	}
 }
+
+	public static BufferedImage MatToBufferedImage(Mat matBGR){  
+		BufferedImage image;
+	    long startTime = System.nanoTime();  
+	    int width = matBGR.width(), height = matBGR.height(), channels = matBGR.channels() ;  
+	    byte[] sourcePixels = new byte[width * height * channels];  
+	    matBGR.get(0, 0, sourcePixels);  
+	    // create new image and get reference to backing data 
+	    if(matBGR.type() == 16){
+	 	   image = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);  
+	    }
+	    else {
+	 	   image = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY);
+	    }
+	    final byte[] targetPixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();  
+	    System.arraycopy(sourcePixels, 0, targetPixels, 0, sourcePixels.length);  
+	   // System.out.println(String.format("Elapsed time: %.2f ms", (float)(endTime - startTime)/1000000));  
+	    return image;  
+	}  
+}
+
